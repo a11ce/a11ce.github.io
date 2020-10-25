4dd1c401d7f91782ac586f8a69ac2eb3e2868f23
a11ce <a11ce@protonmail.com>
2020-10-25 04:12:00 -0400

    Add favicon

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-pandoc README.md -s -H gtag.js -o index.html -c github.css -f markdown --metadata pagetitle="riley (a11ce)'s index"
-pandoc translation.md -s -H gtag.js -o translation.html -c github.css -c translation.css -f markdown --metadata pagetitle="riley (a11ce)'s translations"
+pandoc README.md -s -H headInclude.html -o index.html -c github.css -f markdown --metadata pagetitle="riley (a11ce)'s index"
+pandoc translation.md -s -H headInclude.html -o translation.html -c github.css -c translation.css -f markdown --metadata pagetitle="riley (a11ce)'s translations"
